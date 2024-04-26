99c46f56ddb8250eddda07613e5ca6f45ffdfcae
Joey Maffiola <7maffiolajoey@gmail.com>
2024-04-26 14:04:28 -0500

    Added Models and Context.

---
+++ Models/Song.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace SoundScape.Models;
+
+public partial class Song
+{
+    public string TrackId { get; set; } = null!;
+
+    public string? AlbumId { get; set; }
+
+    public string? TrackName { get; set; }
+
+    public int? Duration { get; set; }
+
+    public string? Genre { get; set; }
+
+    public string? Sample { get; set; }
+
+    public virtual Album? Album { get; set; }
+
+    public virtual ICollection<PlaylistSong> PlaylistSongs { get; set; } = new List<PlaylistSong>();
+}
