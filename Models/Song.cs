d2bbb44c8972f36b3d669f327ba0b268ab3ea2a0
Joey Maffiola <7maffiolajoey@gmail.com>
2024-04-26 12:24:49 -0500

    Added Required tag to AlbumID, TrackName, and Duration.

--- Models/Song.cs
+++ Models/Song.cs
@@ -1,10 +1,17 @@
+using System.ComponentModel.DataAnnotations;
+namespace Models;
 public class Song
 {
+    [Key]
     public int TrackID { get; set; }
+    [Required]
     public int AlbumID { get; set; }
+    [Required]
     public string TrackName { get; set; }
+    [Required]
     public TimeSpan Duration { get; set; }
     public string Genre { get; set; }
+
     public byte[] Sample { get; set; }
 
     // Artist is probably needed here, but is not currently included in this table
