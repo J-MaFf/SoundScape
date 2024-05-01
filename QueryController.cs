59d5ec39cbcc0e474a5551b1bd743787e4d6dff6
Joey Maffiola <7maffiolajoey@gmail.com>
2024-05-01 15:43:18 -0500

    Added new method.

--- QueryController.cs
+++ QueryController.cs
@@ -18,4 +18,21 @@ public class QueryController
             }
         }
     }
+
+    public void ListSongsByAlbum()
+    {
+        using (var context = new CompsciprojectContext())
+        {
+            string albumName = "Californication";
+
+            var songs = context.Albums
+                .Where(s => s.Name == albumName) // Filter songs by album name
+                .ToList(); // Execute the query and get the results as a List
+
+            foreach (var song in songs)
+            {
+                Console.WriteLine(song.Trackname); // Print the name of the song
+            }
+        }
+    }
 }
