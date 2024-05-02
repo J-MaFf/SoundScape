bd5548f06c7a0e6fe6e2d769827150f7f9e8a2eb
TechnoBro03 <matthewanderson.wi@gmail.com>
2024-05-02 17:55:03 -0500

    Brand new project

--- Program.cs
+++ Program.cs
@@ -1,13 +1,18 @@
 namespace SoundScape;
+using System.Threading;
 
-static class Program
+static partial class Program
 {
+    [System.Runtime.InteropServices.LibraryImport("kernel32.dll")]
+    [return: System.Runtime.InteropServices.MarshalAs(System.Runtime.InteropServices.UnmanagedType.Bool)]
+    private static partial bool AllocConsole();
     /// <summary>
     ///  The main entry point for the application.
     /// </summary>
     // [STAThread]
     static void Main()
     {
+        AllocConsole();
         // // To customize application configuration such as set high DPI settings or default font,
         // // see https://aka.ms/applicationconfiguration.
         // ApplicationConfiguration.Initialize();
@@ -17,5 +22,8 @@ static class Program
         QueryController queryController = new();
         // QueryController.ListSongsByArtist();
         QueryController.ListAlbumsBySong("Hello");
+
+        Console.ReadKey(); // Don't end program until user presses key
+        
     }    
 }
