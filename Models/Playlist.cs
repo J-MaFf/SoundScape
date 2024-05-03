eca70d7fdc2b8fa058ea9d92a92eac7bfb4f6081
Joey Maffiola <7maffiolajoey@gmail.com>
2024-05-03 14:50:52 -0500

    Added a playlist constructor.

--- Models/Playlist.cs
+++ Models/Playlist.cs
@@ -5,6 +5,18 @@ namespace COMPSCI366.Models;
 
 public partial class Playlist
 {
+    public Playlist(string playlistId, string? username, string? playlistName, string? description, DateTime? creationDate, ICollection<PlaylistSong> playlistSongs, User? usernameNavigation)
+    {
+
+        PlaylistId = playlistId;
+        Username = username;
+        PlaylistName = playlistName;
+        Description = description;
+        CreationDate = creationDate;
+        PlaylistSongs = playlistSongs;
+        UsernameNavigation = usernameNavigation;
+    }
+
     public string PlaylistId { get; set; } = null!;
 
     public string? Username { get; set; }
