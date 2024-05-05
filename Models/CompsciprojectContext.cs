c95776c2e6b57a6adde6e6a4de78e859c621f00e
Joey Maffiola <7maffiolajoey@gmail.com>
2024-05-04 21:49:17 -0500

    `refactor: Update PlaylistSongId property type to string in PlaylistSong model`

--- Models/CompsciprojectContext.cs
+++ Models/CompsciprojectContext.cs
@@ -26,8 +26,9 @@ public partial class CompsciprojectContext : DbContext
     public virtual DbSet<User> Users { get; set; }
 
     protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
-#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see https://go.microsoft.com/fwlink/?LinkId=723263.
-        => optionsBuilder.UseSqlServer("Server=tcp:compscifinalproject.database.windows.net,1433;Database=compsciproject;User ID=Guestacc1;Password=exbKD9oQ;");
+    {
+        optionsBuilder.UseSqlServer("Server=tcp:compscifinalproject.database.windows.net,1433;Database=compsciproject;User ID=Guestacc1;Password=exbKD9oQ;");
+    }
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
